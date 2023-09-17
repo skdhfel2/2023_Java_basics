e1f62e81d395907e299adf42bca55fa5774c8896
min_e <101707509+skdhfel2@users.noreply.github.com>
2023-09-17 18:37:52 +0900

    Update _11_Package.java

--- chap_07/_11_Package.java
+++ chap_07/_11_Package.java
@@ -6,10 +6,10 @@ public class _11_Package {
     public static void main(String[] args) {
         // 패키지 (랜덤 클래스)
         Random random = new Random();
-        System.out.println("랜덤 정수 : " + random.nextInt()); // int 의 범위 내에서 정수형 값 반환
+        System.out.println("랜덤 정수 : " + random.nextInt()); // int 의 범위 내에서 무작위 정수값 반환
         System.out.println("랜덤 정수 (범위) : " + random.nextInt(10)); // 0 이상 10 미만의 정수형 값
         System.out.println("랜덤 실수 : " + random.nextDouble()); // 0.0 이상 1.0 미만의 실수값
-        // System.out.println("랜덤 실수 (범위) : " + random.nextDouble(10.0)); //Double은 값을 못넣는다.
+        // System.out.println("랜덤 실수 (범위) : " + random.nextDouble(10.0)); // Double은 값을 못넣는다.
 
         // 만약 5.0 이상 10.0 미만의 실수를 뽑으려면?
         double min = 5.0;
