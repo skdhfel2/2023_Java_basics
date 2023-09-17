2461687360f00fe45042812bf0728b0d380f7d0e
min_e <101707509+skdhfel2@users.noreply.github.com>
2023-09-17 20:46:55 +0900

    Update Camera.java

--- chap_07/camera/Camera.java
+++ chap_07/camera/Camera.java
@@ -1,32 +1,28 @@
-package chap_07;
+package chap_07.camera;
 
-public class _01_Class {
-    public static void main(String[] args) {
-        // 객체 지향 프로그래밍 (OOP : Object-Oriented Programming)
-        // 유지보수 용이
-        // 높은 재사용성
+public class Camera { // 부모 클래스
+    public String name;
 
-        // 차량용 블랙박스
-        // 모델명, 해상도, 가격, 색상
+    public Camera() {
+        this("카메라"); // 현재 클래스에서 다른 생성자를 호출하는 코드, 생성자 체이닝, String을 인수로 받는 생성자 호출
+    } // protected Camera(String name) 생성자를 호출한다.
+    // 생성자 : 메소드
 
-        // 우리가 만든 첫 번째 제품
-        String modelName = "까망이";
-        String resolution = "FHD";
-        int price = 200000;
-        String color = "블랙";
+    protected Camera(String name) {
+        this.name = name;
+    }
 
-        // 새로운 제품을 개발
-        String modelName2 = "하양이";
-        String resolution2 = "UHD";
-        int price2 = 300000;
-        String color2 = "화이트";
+    public void takePicture() {
+        // 사진 촬영
+        System.out.println(this.name + " : 사진을 촬영합니다.");
+    }
 
-        // 또다른 제품을 개발?
-        BlackBox bbox = new BlackBox();
-        // BlackBox 클래스로부터 bbox 객체 생성
-        // bbox 객체는 BlackBox 클래스의 인스턴스, 인스턴스 & 객체 : 비슷한 뜻
-        // 클래스 : 설계도 ,객체 : 설계도를 통해 만든 실체, 인스턴스 : 설계도를 통해 만든 실체 데이터
+    public void recordVideo() {
+        // 동영상 녹화
+        System.out.println(this.name + " : 동영상을 녹화합니다.");
+    }
 
-        BlackBox bbox2 = new BlackBox();
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 사진 촬영, 동영상 녹화");
     }
 }
