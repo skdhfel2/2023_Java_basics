038121bcae45518846342c5d4dc9130d042d1d75
min_e <101707509+skdhfel2@users.noreply.github.com>
2023-10-08 15:10:57 +0900

    Delete VideoReporter.java

--- VideoReporter.java
+++
@@ -1,8 +0,0 @@
-package chap_08.reporter;
-
-public class VideoReporter implements Reportable {
-    @Override
-    public void report() {
-        System.out.println("직전 30초 영상과 함께 신고를 진행합니다.");
-    }
-}
