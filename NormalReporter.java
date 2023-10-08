a519299c75d9f86da49d76b6de898a4a48ca96e7
min_e <101707509+skdhfel2@users.noreply.github.com>
2023-10-08 15:10:45 +0900

    Delete NormalReporter.java

--- NormalReporter.java
+++
@@ -1,8 +0,0 @@
-package chap_08.reporter;
-
-public class NormalReporter implements Reportable { // implements : 구현하다
-    @Override
-    public void report() {
-        System.out.println("일반 화재 신고를 진행합니다.");
-    }
-}
