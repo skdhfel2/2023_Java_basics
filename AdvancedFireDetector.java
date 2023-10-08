c0bd830d56f62c665aa8a68fe3b314e5f37663c7
min_e <101707509+skdhfel2@users.noreply.github.com>
2023-10-08 15:09:56 +0900

    Delete AdvancedFireDetector.java

--- AdvancedFireDetector.java
+++
@@ -1,8 +0,0 @@
-package chap_08.detector;
-
-public class AdvancedFireDetector implements Detectable{
-    @Override
-    public void detect() {
-        System.out.println("향상된 성능으로 화재를 감지합니다.");
-    }
-}
